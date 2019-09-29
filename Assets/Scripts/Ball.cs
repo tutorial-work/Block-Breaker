dc89e134aa6acbec4ce411c72655b67bf3e537db
Buddhamon <wlacey@smu.edu>
2019-09-28 23:15:07 -0500

    Adding Ball, Paddle, and Bottom Screne Lose Trigger

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // config params
+    [SerializeField] Paddle paddle;
+    bool hasStarted = false;
+
+    // state
+    Vector2 paddleToBallVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+    
+    private void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+            hasStarted = true;
+        }
+    }
+
+    private void LockToPaddle()
+    {
+        Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePosition + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidth = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidth;
+        mousePositionInUnits = Mathf.Clamp(mousePositionInUnits, 0, screenWidth);
+
+        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        transform.position = paddlePosition;
+    }
+}
